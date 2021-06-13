f091b0813be4a737044b3ec3a125dd28144190db
jatinudasi <jatin.udasi88@gmail.com>
2021-06-13 23:58:29 +0530

    form update

--- src/Form.js
+++ src/Form.js
@@ -1,35 +1,35 @@
-// import React,{useState} from 'react'
-// import axios from 'axios'
+import React,{useState} from 'react'
+import axios from 'axios'
 
-// function Form(props) {
+function Form(props) {
 
-//     const [username, setUsername] = useState('')
+    const [username, setUsername] = useState('')
 
-//  const handleSubmit = event => {
-//     event.preventDefault()
+ const handleSubmit = event => {
+    event.preventDefault()
 
-//     axios
-//       .get(`https://api.github.com/users/${username}`)
-//       .then(resp => {
-//         props.onSubmit(resp.data)
-//         setUsername('')
-//       })
-//       .catch(err=>console.log(err))
-//     }
+    axios
+      .get(`https://api.github.com/users/${username}`)
+      .then(resp => {
+        props.onSubmit(resp.data)
+        setUsername('')
+      })
+      .catch(err=>console.log(err))
+    }
 
 
-//     return (
-//         <form onSubmit={handleSubmit}>
-//           <input
-//             type="text"
-//             value={username}
-//             onChange={event => setUsername(event.target.value)}
-//             placeholder="GitHub username"
-//             required
-//           />
-//           <button type="submit">Add card</button>
-//         </form>
-//       )
-// }
+    return (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={username}
+            onChange={event => setUsername(event.target.value)}
+            placeholder="GitHub username"
+            required
+          />
+          <button type="submit">Add card</button>
+        </form>
+      )
+}
 
-// export default Form
+export default Form
